3a8723e0fa33c9e42536f2cdd5e0bd5bf008aad4
arsen <student@ALEM-F4-R02-06.alem.school>
2025-03-06 12:43:07 +0500

    feat: mirror

--- internal/flags/readFlags.go
+++ internal/flags/readFlags.go
@@ -7,27 +7,64 @@ import (
 )
 
 type Config struct {
-	Command    string
-	Filename   string
-	MirrorType string
-	Filter     string
+	Command     string
+	Filename    string
+	NewFileName string
+	MirrorType  []string // Slice to store multiple mirroring options
+	Filter      string
+	Help        bool
 }
 
 func ReadFlags() Config {
 	var cfg Config
 
-	flag.StringVar(&cfg.Command, "command", "", "Command to execute (header, copy)")
-	flag.StringVar(&cfg.Filename, "filename", "", "Filename to process")
-	flag.StringVar(&cfg.MirrorType, "mirror", "", "Mirroring options (horizontal, vertical)")
+	flag.StringVar(&cfg.Command, "", "", "Command to execute (header, copy, apply)")
+	flag.StringVar(&cfg.Filename, "filename", "", "Source file to process")
+	flag.StringVar(&cfg.NewFileName, "newfilename", "", "Output file for processed image")
+	flag.Var((*stringSliceFlag)(&cfg.MirrorType), "mirror", "Mirroring options (horizontal, vertical). Can be specified multiple times.")
 	flag.StringVar(&cfg.Filter, "filter", "", "Filter to apply on bitmap")
+	flag.BoolVar(&cfg.Help, "help", false, "Prints usage information")
 
+	// Default usage (for ./bitmap)
 	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, "Usage: go run [options]<filename>\n")
-		fmt.Fprintln(os.Stderr, "Options:")
-		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "Usage:\n")
+		fmt.Fprintf(os.Stderr, "  bitmap <command> [arguments]\n\n")
+		fmt.Fprintf(os.Stderr, "The commands are:\n")
+		fmt.Fprintf(os.Stderr, "  header    prints bitmap file header information\n")
+		fmt.Fprintf(os.Stderr, "  copy      copies the image to a new file\n")
+		fmt.Fprintf(os.Stderr, "  apply     applies processing to the image and saves it to the file\n\n")
+		fmt.Fprintf(os.Stderr, "Use 'bitmap <command> --help' for more information about a command.\n")
 	}
 
 	flag.Parse()
+
+	// If --help is passed, show command-specific usage
+	if cfg.Help {
+		switch cfg.Command {
+		case "header":
+			fmt.Fprintf(os.Stderr, "Usage:\n")
+			fmt.Fprintf(os.Stderr, "  bitmap header <source_file>\n\n")
+			fmt.Fprintf(os.Stderr, "Description:\n")
+			fmt.Fprintf(os.Stderr, "  Prints bitmap file header information\n")
+			os.Exit(0)
+		case "copy":
+			fmt.Fprintf(os.Stderr, "Usage:\n")
+			fmt.Fprintf(os.Stderr, "  bitmap copy <source_file> <output_file>\n\n")
+			fmt.Fprintf(os.Stderr, "Description:\n")
+			fmt.Fprintf(os.Stderr, "  Copies the image to a new file\n")
+			os.Exit(0)
+		case "apply":
+			fmt.Fprintf(os.Stderr, "Usage:\n")
+			fmt.Fprintf(os.Stderr, "  bitmap apply [options] <source_file> <output_file>\n\n")
+			fmt.Fprintf(os.Stderr, "The options are:\n")
+			fmt.Fprintf(os.Stderr, "  --mirror        mirroring options (horizontal, vertical). Can be specified multiple times.\n")
+			fmt.Fprintf(os.Stderr, "  --filter        filter to apply on bitmap\n")
+			os.Exit(0)
+		default:
+			flag.Usage()
+			os.Exit(0)
+		}
+	}
 
 	// Validate required flags
 	if cfg.Command == "" || cfg.Filename == "" {
@@ -36,27 +73,33 @@ func ReadFlags() Config {
 		os.Exit(1)
 	}
 
-	// Validate command
+	// Validate command-specific flags
 	switch cfg.Command {
-	case "header", "copy":
-		// Valid commands
+	case "header":
+		// No additional flags required
+	case "copy", "apply":
+		if cfg.NewFileName == "" {
+			fmt.Fprintln(os.Stderr, "Error: -newfilename is required for this command")
+			flag.Usage()
+			os.Exit(1)
+		}
 	default:
 		fmt.Fprintln(os.Stderr, "Error: invalid command")
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	// Validate mirror type
-	if cfg.MirrorType != "" {
-		switch cfg.MirrorType {
-		case "horizontal", "h", "vertical", "v":
-			// Valid options
-		default:
-			fmt.Fprintln(os.Stderr, "Error: invalid mirror type")
-			flag.Usage()
-			os.Exit(1)
-		}
-	}
-
 	return cfg
+}
+
+// stringSliceFlag is a custom flag type to support multiple values for the same flag.
+type stringSliceFlag []string
+
+func (s *stringSliceFlag) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *stringSliceFlag) Set(value string) error {
+	*s = append(*s, value)
+	return nil
 }
--- internal/tools/copy.go
+++ internal/tools/copy.go
@@ -2,68 +2,35 @@ package tools
 
 import (
 	"fmt"
-	"os"
 )
 
-func (bm *Bitmap) Copy() error {
-	file, err := os.Create("result.bmp")
+func (bm *Bitmap) Copy(newfilename string) error {
+	file, err := bm.copyHeader(newfilename)
 	if err != nil {
-		return fmt.Errorf("failed to create BMP file: %w", err)
+		return err
 	}
-	defer file.Close()
-
-	h := int(bm.Header.Height)
-	if h < 0 {
-		h = -h
-	}
-	w := int(bm.Header.Width)
-
-	// Write the BMP header
-	header := make([]byte, 54)
-	header[0] = 'B'
-	header[1] = 'M'
-	writeUint32(header[2:6], bm.Header.FileSize)
-	writeUint32(header[6:10], bm.Header.Reserved)
-	writeUint32(header[10:14], bm.Header.PixelArrayOffset)
-	writeUint32(header[14:18], bm.Header.DIBHeaderSize)
-	writeUint32(header[18:22], bm.Header.Width)
-	writeInt32(header[22:26], bm.Header.Height)
-	writeUint16(header[26:28], bm.Header.ColorPlanes)
-	writeUint16(header[28:30], bm.Header.BitsPerPixel)
-	writeUint32(header[30:34], bm.Header.Compression)
-	writeUint32(header[34:38], bm.Header.ImageSize)
-	writeUint32(header[38:42], bm.Header.XResolution)
-	writeUint32(header[42:46], bm.Header.YResolution)
-	writeUint32(header[46:50], bm.Header.ColorsInPalette)
-	writeUint32(header[50:54], bm.Header.ImportantColors)
-
-	_, err = file.Write(header)
-	if err != nil {
-		return fmt.Errorf("failed to write header to BMP file: %w", err)
-	}
-
-	// Write pixel data
-	bytesPerPixel := int(bm.Header.BitsPerPixel) / 8
-	rowSize := w * bytesPerPixel
-	paddingSize := (4 - (rowSize % 4)) % 4 // Padding to make row size a multiple of 4
-	padding := make([]byte, paddingSize)
+	h := int(bm.Px.H)
+	rowSize := int(bm.Px.RowSize)
+	padSize := int(bm.Px.PadSize)
+	pad := bm.Px.Pad
+	pxdata := bm.Px.Data
 
 	for y := 0; y < h; y++ { // BMP stores rows bottom-to-top
 		rowStart := y * rowSize
 		rowEnd := rowStart + rowSize
-		row := bm.PixelData[rowStart:rowEnd]
+		row := pxdata[rowStart:rowEnd]
 
-		_, err = file.Write(row)
 		if err != nil {
 			return fmt.Errorf("failed to write pixel data to BMP file: %w", err)
 		}
 
 		// Write padding bytes if necessary
-		if paddingSize > 0 {
-			_, err = file.Write(padding)
+		if padSize > 0 {
+			_, err = file.Write(pad)
 			if err != nil {
 				return fmt.Errorf("failed to write padding to BMP file: %w", err)
 			}
+			_, err = file.Write(row)
 		}
 	}
 
