9b3747aeee344fe810a78e7b19e959b2b4e68c54
arsen <student@ALEM-F4-R02-06.alem.school>
2025-03-06 19:09:30 +0500

    feat: image processing algorithms

--- main.go
+++ main.go
@@ -31,21 +31,55 @@ func main() {
 			return
 		}
 	case "apply":
-		// Validate mirror type
-		mirror := new(tools.Mirror)
-		for _, mtype := range cfg.MirrorType {
-			switch mtype {
-			case "horizontal", "h", "horizontally", "hor":
-				mirror.H = true
-			case "vertical", "v", "vertically", "ver":
-				mirror.V = true
-			default:
-				fmt.Fprintln(os.Stderr, "Error: invalid mirror type")
-				flag.Usage()
-				os.Exit(1)
+		if len(cfg.MirrorType) != 0 {
+
+			// Validate mirror type
+			mirror := new(tools.Mirror)
+			for _, mtype := range cfg.MirrorType {
+				if mirror.H && mirror.V {
+					break
+				}
+				switch mtype {
+				case "horizontal", "h", "horizontally", "hor":
+					mirror.H = true
+				case "vertical", "v", "vertically", "ver":
+					mirror.V = true
+				default:
+					fmt.Fprintln(os.Stderr, "Error: invalid mirror type")
+					flag.Usage()
+					os.Exit(1)
+				}
 			}
+			bm.Mirror(cfg.NewFileName, mirror)
 		}
-		bm.Mirror(cfg.NewFileName, mirror)
+		if len(cfg.FilterType) != 0 {
+
+			// Validate mirror type
+			filter := new(tools.Filter)
+			for _, ftype := range cfg.FilterType {
+				switch ftype {
+				case "blue":
+					filter.IsBlue = true
+				case "red":
+					filter.IsRed = true
+				case "green":
+					filter.IsGreen = true
+				case "negative":
+					filter.IsNegative = true
+				case "pixelate":
+					filter.IsPixelate = true
+				case "blur":
+					filter.IsBlur = true
+				default:
+					fmt.Fprintln(os.Stderr, "Error: invalid filter type")
+					flag.Usage()
+					os.Exit(1)
+				}
+			}
+			bm.Filter(cfg.NewFileName, filter)
+
+		}
+
 	default:
 		fmt.Println("Unknown command:", cfg.Command)
 	}
